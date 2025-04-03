62c7bfc971d5aa0e4dde18a6db3ee8950567038f
Jake Soenneker <jake@soenneker.com>
2025-04-03 16:55:57 -0500

    Initial commit

---
+++ src/LoggerExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using Soenneker.Extensions.DateTime;
+
+namespace Soenneker.Extensions.Logger;
+
+/// <summary>
+/// A collection of helpful ILogger extension methods
+/// </summary>
+public static class LoggerExtension
+{
+    public static void LogStartupInformation(this ILogger logger, System.TimeZoneInfo timeZoneInfo)
+    {
+        logger.LogDebug("----- Startup information -----");
+
+        logger.LogDebug("Current time: {time} ET", System.DateTime.UtcNow.ToTz(timeZoneInfo).ToPreciseFormat());
+
+        logger.LogDebug("----- /Startup information -----");
+    }
+}
---
+++ test/Soenneker.Extensions.Logger.Tests/LoggerExtensionTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Logger.Tests;
+
+public sealed class LoggerExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
