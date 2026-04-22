38042c3aa56bf6cd1f0e6987f98092895f71160e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:00 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Logger.Tests/LoggerExtensionTests.cs
+++ test/Soenneker.Extensions.Logger.Tests/LoggerExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.Logger.Tests;
 
 public sealed class LoggerExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
